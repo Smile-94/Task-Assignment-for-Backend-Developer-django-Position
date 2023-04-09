cabfe7b47598bd607e38df990f12306be6548376
Sazzad Hossen <mshossen75@gmail.com>
2023-04-09 17:57:05 +0600

    Finilize the project of blog post

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Task-Assignment-for-Backend-Developer-django-Position
+Setup
+The first thing to do is to clone the repository:
+
+$ git clone https://github.com/Smile-94/Task-Assignment-for-Backend-Developer-django-Position.git
+Create a virtual environment to install dependencies in and activate it:
+
+$ virtualenv venv
+$ source venv/bin/activate
+Then install the dependencies:
+
+(env)$ pip install -r requirements.txt
--- config/settings.py
+++ config/settings.py
@@ -88,24 +88,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Setting for postgresql
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "careBoxDb", # Database Name
-#         "USER": "postgres",
-#         "PASSWORD": "your password", # Create custom databse password
-#         "HOST": "localhost",
-#         "PORT": 5432,
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# Setting for postgresql
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "careBoxDb", # Database Name
+        "USER": "postgres",
+        "PASSWORD": "your password", # Create custom databse password
+        "HOST": "localhost",
+        "PORT": 5432,
+    }
+}
 
 
 
